56b28df8ea27cbb00da7fe1df2825883579152ae
Konrad Rodzik <konrad.rodzik@gmail.com>
2015-02-23 02:04:50 +0100

    version 1.0.0

--- QordobaSDK.framework/Versions/A/Headers/QordobaSDK.h
+++ QordobaSDK.framework/Versions/A/Headers/QordobaSDK.h
@@ -2,18 +2,20 @@
 //  QordobaSDK.h
 //  QordobaSDK
 //
-//  Created by Konrad Rodzik on 1/26/15.
+//  Created by Qordoba on 1/26/15.
 //  Copyright (c) 2015 Qordoba. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "QSDKConfiguration.h"
 
-@interface QordobaSDK : NSObject
-+ (void)startWithKey:(NSString *)key andProjectId:(NSString *)projectId;
+@interface QordobaSDK : NSObject<UIAlertViewDelegate>
++ (void)startWithKey:(NSString *)key andApplicationId:(NSString *)applicationId;
++ (void)startWithKey:(NSString *)key andApplicationId:(NSString *)applicationId andAutoInitialization:(BOOL)flag;
 + (void)startWithConfiguration:(QSDKConfiguration*)configuration;
 
 + (BOOL)isSandboxOn;
-
 + (QSDKConfiguration *)getSDKConfiguration;
+
++ (BOOL)isValidController:(UIViewController *)viewController;
 @end
